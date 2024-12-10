be8f87142a6c37fccdf54e76223f703b92983a31
developer.tanish <tgangan2003@gmail.com>
2024-12-10 13:08:02 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require ('cors')
+
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+});
+
+module.exports = app;
